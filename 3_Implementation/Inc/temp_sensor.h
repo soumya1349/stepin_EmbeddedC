4060b8a39b830909e59196adb4bfa9134a96af1c
soumya1349 <soumyassureban@gmail.com>
2021-09-16 18:20:44 +0530

    Updated

---
+++ 3_Implementation/Inc/temp_sensor.h
@@ -0,0 +1,16 @@
+/**
+ * @file temp_sensor.h
+ * @author Soumya
+ * @brief Defination of temperature sensor
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef TEMP_SENSOR_H
+#define TEMP_SENSOR_H
+uint16_t ReadADC(uint8_t ch);
+
+
+#endif // TEMP_SENSOR_H_INCLUDED
