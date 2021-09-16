eb6169d5aaf3428d260ee7e7e2fed349403c9270
soumya1349 <soumyassureban@gmail.com>
2021-09-16 18:20:57 +0530

    Updated

---
+++ 3_Implementation/Inc/uart.h
@@ -0,0 +1,18 @@
+/**
+ * @file uart.h
+ * @author your name (you@domain.com)
+ * @brief UART Display
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef UART_H
+#define UART_H
+void USARTInit(uint16_t );
+char USARTReadChar();
+void USARTWriteChar(char );
+
+
+#endif // UART_H_INCLUDED
